2c66219e9a3f92c34c3e154f4d73b5a68402689c
aliSajadi <157576611+aliSajadi01@users.noreply.github.com>
2024-08-07 02:18:43 +0330

    Complet of the weather mini project

--- src/app/axiosConfig/axios.js
+++ src/app/axiosConfig/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
 let Axios = axios.create({
   baseURL: 'https://api.openweathermap.org/',
 });
+
 export default Axios;
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,178 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from 'react';
+import { TiWeatherSunny } from 'react-icons/ti';
+import { TbTemperatureCelsius } from 'react-icons/tb';
+import { LuCloudFog } from 'react-icons/lu';
+import { IoRainyOutline } from 'react-icons/io5';
+import { MdOutlineWindPower } from 'react-icons/md';
+import moment from 'moment';
+import Axios from './axiosConfig/axios';
 
 export default function Home() {
+  let [search, setSearch] = useState('');
+  let [weather, setWeather] = useState({});
+  let [loading, setLoading] = useState(false);
+  let [currentTime, setCurrentTime] = useState('');
+  let [hors, setHors] = useState(Date.now());
+  let [nightOrDay, setNigtOrDay] = useState(false);
+  let sunrise = weather?.sys?.sunrise * 1000;
+  let sunset = weather?.sys?.sunset * 1000;
+  useEffect(() => {
+    setNigtOrDay(hors < sunrise || hors > sunset);
+  }, [sunset, sunrise]);
+  let getTime = () => {
+    return moment().format('YYYY/MM/DD - HH:mm');
+  };
+  useEffect(() => {
+    let nowTime = getTime();
+    setCurrentTime(nowTime);
+    let interval = setInterval(() => {
+      setCurrentTime(getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+  let getWeather = async () => {
+    try {
+      setLoading(true);
+      let data = await Axios.get('data/2.5/weather', {
+        params: {
+          q: search,
+          appid: process.env.NEXT_PUBLIC_API_WEATHER,
+        },
+      });
+      setSearch('');
+      if (data) {
+        setWeather(data.data);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error.message);
+    }
+  };
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    getWeather();
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=" h-[100vh] w-full relative flex items-center justify-center ">
+      {Object.keys(weather).length > 0 ? (
+        <>
+          {nightOrDay ? (
+            <>
+              <video
+                className=" h-full object-cover w-full"
+                src={
+                  weather.weather[0].main === 'Clear'
+                    ? '../../videos/2735069-uhd_3240_2160_15fps.mp4'
+                    : weather.weather[0].main === 'Clouds'
+                    ? '../../videos/854739-hd_1920_1080_30fps.mp4'
+                    : weather.weather[0].main === 'Rain'
+                    ? '../../videos/4323285-hd_1920_1080_30fps.mp4'
+                    : ''
+                }
+                autoPlay
+                loop
+                muted
+              />
+            </>
+          ) : (
+            <>
+              <video
+                className=" h-full object-cover w-full"
+                src={
+                  weather.weather[0].main === 'Clear'
+                    ? '../../videos/7498095-uhd_3840_2160_30fps.mp4'
+                    : weather.weather[0].main === 'Clouds'
+                    ? '../../videos/855785-hd_1920_1080_24fps.mp4'
+                    : weather.weather[0].main === 'Rain'
+                    ? '../../videos/3813820-hd_1920_1080_24fps.mp4'
+                    : ''
+                }
+                autoPlay
+                loop
+                muted
+              />
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <img
+            className="h-full object-cover w-full"
+            src="../../videos/pexels-biluk-165537.jpg"
+            alt=""
+          />
+        </>
+      )}
+
+      <div className="absolute backdrop-blur-sm md:w-[500px] md:h-[600px] h-[80%] w-[90%] bg-white rounded-lg bg-opacity-10 flex flex-col  justify-center items-center md:p-10 p-5">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex justify-center items-center gap-3">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            type="text"
+            placeholder="Search..."
+            className=" w-[70%] outline-none border border-blue-300 rounded-full md:p-2 p-1 flex justify-center items-center"
+          />
+          <button className="md:p-2 p-1 rounded-full bg-blue-600 text-white font-normal">
+            {loading ? 'Searching...' : 'Search'}
+          </button>
+        </form>
+        {Object.keys(weather).length > 0 && (
+          <p className="text-9xl flex items-center justify-center text-blue-600 font-bold ">
+            {Math.floor(weather?.main?.temp - 273.15)}
+            <TbTemperatureCelsius />
+          </p>
+        )}
+        <div>
+          {Object.keys(weather).length > 0 && (
+            <div className={`flex items-center justify-center ${nightOrDay ? "text-white" : ""} `}>
+              <p className="text-2xl flex items-center justify-center  font-mono ">
+                {weather.name}
+              </p>
+              <p className="text-2xl flex items-center justify-center  font-mono ">
+                -{weather.sys.country}
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="w-full h-full mt-10 p-5">
+          {Object.keys(weather).length > 0 && (
+            <div className="flex items-center justify-between">
+              {weather.weather[0].main === 'Clear' ? (
+                <div className="flex flex-col items-center">
+                  <TiWeatherSunny className="size-32 text-blue-900 " />
+                  <p className="text-2xl font-mono text-blue-900">Clear</p>
+                </div>
+              ) : weather.weather[0].main === 'Clouds' ? (
+                <div className="flex flex-col items-center">
+                  <LuCloudFog className="size-32 text-blue-900 " />
+                  <p className="text-2xl font-mono text-blue-900">Clouds</p>
+                </div>
+              ) : weather.weather[0].main === 'Rain' ? (
+                <div className="flex flex-col items-center">
+                  <IoRainyOutline className="size-32 text-blue-900 " />
+                  <p className="text-2xl font-mono text-blue-900">Rain</p>
+                </div>
+              ) : (
+                ''
+              )}
+              <div className="flex flex-col items-center font-mono">
+                <MdOutlineWindPower className="size-32 text-blue-900 " />
+                <p className="text-2xl text-blue-900">{weather.wind.speed}km</p>
+              </div>
+            </div>
+          )}
+        </div>
+        <div>
+          <p className={`font-bold underline ${nightOrDay ? "text-white" : ""}`}>{currentTime}</p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
